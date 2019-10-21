810750f0c087bb50a739fdf0702e48cafd2455b3
tfylee98 <56703109+tfylee98@users.noreply.github.com>
2019-10-21 16:37:01 -0700

    ch04

---
+++ ch04/Zool.java
@@ -0,0 +1,9 @@
+public class Zool {
+    public static void zool(int num, String str1, String str2) {
+        System.out.printf("%d, %s, %s\n", num, str1, str2);
+    }
+    
+    public static void main(String[] args){
+        zool(11, "Gare", "North Star");
+    }
+}
